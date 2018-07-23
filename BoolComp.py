f823b6614a809cd2664e4ec8f75b270505e257c6
Ladydiana <diana.culincu@gmail.com>
2018-07-23 21:23:18 +0300

    Booleans and Comparisons.

---
+++ BoolComp.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+BOOLEANS AND COMPARISONS
+"""
+
+sf_population, sf_area = 864816, 231.89
+rio_population, rio_area = 6453682, 486.5
+
+san_francisco_pop_density = sf_population/sf_area
+rio_de_janeiro_pop_density = rio_population/rio_area
+
+# Write code that prints True if San Francisco is denser than Rio, and False otherwise
+if (san_francisco_pop_density>rio_de_janeiro_pop_density):
+    print(True)
+else:
+    print(False)
+#or like this which is more concise in this case
+print(san_francisco_pop_density > rio_de_janeiro_pop_density)
+
