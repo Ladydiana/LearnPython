e5c169b196a805abb9fb2d79cdf3af1e117d2f66
Ladydiana <diana.culincu@gmail.com>
2018-07-25 23:19:54 +0300

    Keybord input.

---
+++ Input.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+INPUT
+"""
+
+name = input("Enter your name: ")
+print("Hello there, {}!".format(name.title()))
+
+result = eval(input("Enter an expression: "))
+print(result)
