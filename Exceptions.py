024c2f4b3cb0a8610991a77fdee1ddc4bc5468fd
Ladydiana <diana.culincu@gmail.com>
2018-07-25 23:19:22 +0300

    Exceptions.

---
+++ Exceptions.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+EXCEPTIONS
+"""
+
+"""
+try:
+    # some code
+except ValueError:
+    # some code
+
+try:
+    # some code
+except ValueError, KeyboardInterrupt:
+    # some code
+
+try:
+    # some code
+except ValueError:
+    # some code
+except KeyboardInterrupt:
+    # some code
+    
+try:
+    # some code
+except Exception as e:
+   # some code
+   print("Exception occurred: {}".format(e))
+"""    
+    
+def party_planner(cookies, people):
+    leftovers = None
+    num_each = None
+    # TODO: Add a try-except block here to
+    #       make sure no ZeroDivisionError occurs.
+    try:
+     num_each = cookies // people
+     leftovers = cookies % people
+    except ZeroDivisionError:
+     print("Division by zero.")
+   
+
+    return(num_each, leftovers)
+
+# The main code block is below; do not edit this
+lets_party = 'y'
+while lets_party == 'y':
+
+    cookies = int(input("How many cookies are you baking? "))
+    people = int(input("How many people are attending? "))
+
+    cookies_each, leftovers = party_planner(cookies, people)
+
+    if cookies_each:  # if cookies_each is not None
+        message = "\nLet's party! We'll have {} people attending, they'll each get to eat {} cookies, and we'll have {} left over."
+        print(message.format(people, cookies_each, leftovers))
+
+    lets_party = input("\nWould you like to party more? (y or n) ")
